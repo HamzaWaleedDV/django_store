f5a55417c1f01cfd35160d7667a96c3697700178
Hamza Waleed <hamzawaleed.grant@gmail.com>
2023-08-01 16:49:45 +0300

    add templates and views

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryModel(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Author)
+class AuthorModel(admin.ModelAdmin):
+    list_per_page = 20
+
+
+@admin.register(models.Product)
+class ProductModel(admin.ModelAdmin):
+    list_per_page = 20    
+
+
+
+@admin.register(models.Slider)
+class SliderModel(admin.ModelAdmin):
+    list_per_page = 20
